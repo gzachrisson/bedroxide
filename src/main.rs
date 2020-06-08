3f95f36e3effcb418946476d3dad4b8825d187c2
Göran Zachrisson <g.zachrisson@gmail.com>
2020-06-08 23:54:06 +0200

    Initialize logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use log::info;
+use simplelog::*;
+use std::fs::File;
+
 fn main() {
-    println!("Bedroxide started!");
+    CombinedLogger::init(
+        vec![
+            SimpleLogger::new(LevelFilter::Info, Config::default()),
+            WriteLogger::new(LevelFilter::Info, Config::default(), File::create("bedroxide.log").unwrap()),
+        ]
+    ).unwrap();
+
+    info!("Bedroxide started!");
 }
