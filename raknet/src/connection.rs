bef1e85c15ccc9b49f88fb692e1ea39bf6c631bc
Göran Zachrisson <g.zachrisson@gmail.com>
2021-04-28 00:59:00 +0200

    Add some todos

--- raknet/src/connection.rs
+++ raknet/src/connection.rs
@@ -49,14 +49,24 @@ impl Connection {
             Ok(DatagramHeader::Ack { data_arrival_rate }) => {
                 debug!("Received ACK. data_arrival_rate={:?}", data_arrival_rate);
                 // TODO: Send ACK receipt to user for unreliable packets with ACK receipt requested (and remove packets from list)
-                // TODO: Remove ACK:ed packets from resend list (and send ACK receipt to user) (and delete older reliable sequenced packets (not needed?))
+                // TODO: Remove ACK:ed packets from resend list (and send ACK receipt to user)
             },
             Ok(DatagramHeader::Nack) => {
                 debug!("Received NACK");
+                // TODO: Resend NACK:ed datagrams (by setting the next resend time to current time so they will be sent in next update)
             },
             Ok(DatagramHeader::Packet {is_packet_pair, is_continuous_send, needs_data_arrival_rate, datagram_number }) => {
                 debug!("Received a datagram of packets. is_packet_pair={}, is_continuous_send={}, needs_data_arrival_rate={}, datagram_number={}", 
                 is_packet_pair, is_continuous_send, needs_data_arrival_rate, datagram_number);
+                // TODO: Schedule NACKs on missed datagrams to be sent next update
+                // TODO: Schedule ACK on the received datagram to be sent next update (if it is time to send ACKs then)
+                // TODO: Parse the list of packages in the datagram.
+                // TODO: For each package:
+                // TODO: - If reliable: Check if the reliable message number is the expected. Update holes and the expected. Drop if it is a duplicate.
+                // TODO: - If split packet count > 0: Reassemble the packet and send download progress to the user.
+                // TODO:   If not fully reassembled then continue with next packet.
+                // TODO: - If sequenced or ordered: Check if the order or sequence is correct so the packet can be delivered to the user. Buffer it otherwise.
+                // TODO: - Send the packet to the user unless otherwise stated 
             },
             Err(err) => error!("Error parsing datagram header: {:?}", err),
         };
