6ae2e74b16876599258087fd774f9f12a15bfbd5
Göran Zachrisson <g.zachrisson@gmail.com>
2021-05-11 21:49:10 +0200

    Add test of OutgoingAcknowledgements

--- raknet/src/acknowledgement.rs
+++ raknet/src/acknowledgement.rs
@@ -54,7 +54,7 @@ impl OutgoingAcknowledgements {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct AcknowledgementRange {
     start: DatagramSequenceNumber,
     end: DatagramSequenceNumber,
@@ -158,9 +158,9 @@ impl Acknowledgement {
 
 #[cfg(test)]
 mod tests {
-    use std::convert::TryFrom;
-    use crate::DatagramSequenceNumber;
-    use super::{Acknowledgement, AcknowledgementRange};
+    use std::{convert::TryFrom, time::{Duration, Instant}};
+    use crate::{constants::TIME_BEFORE_SENDING_ACKS, DatagramSequenceNumber};
+    use super::{Acknowledgement, AcknowledgementRange, OutgoingAcknowledgements};
 
     #[test]
     fn acknowledgement_write_one_range_one_ack() {
@@ -328,4 +328,143 @@ mod tests {
         assert_eq!(range.start(), 7u8.into());
         assert_eq!(range.end(), DatagramSequenceNumber::try_from(0xFFFFFFu32).unwrap());
     }
+
+    #[test]
+    fn outgoing_acks_is_empty_initial_state_empty() {
+        // Arrange
+        let acks = OutgoingAcknowledgements::new();
+
+        // Act/Assert
+        assert!(acks.is_empty());
+    }
+
+    #[test]
+    fn outgoing_acks_is_empty_empty() {
+        // Arrange
+        let mut acks = OutgoingAcknowledgements::new();
+        acks.insert(DatagramSequenceNumber::from(5u8), Instant::now());
+        acks.pop_range();
+
+        // Act/Assert
+        assert!(acks.is_empty());
+    }
+
+    #[test]
+    fn outgoing_acks_is_empty_not_empty() {
+        // Arrange
+        let mut acks = OutgoingAcknowledgements::new();
+        acks.insert(DatagramSequenceNumber::from(5u8), Instant::now());
+
+        // Act/Assert
+        assert!(!acks.is_empty());
+    }
+
+    #[test]
+    fn outgoing_acks_pop_range_empty() {
+        // Arrange
+        let mut acks = OutgoingAcknowledgements::new();
+
+        // Act/Assert
+        assert_eq!(acks.pop_range(), None);
+    }
+
+    #[test]
+    fn outgoing_acks_pop_range_one_range_start_end_same() {
+        // Arrange
+        let mut acks = OutgoingAcknowledgements::new();
+        acks.insert(DatagramSequenceNumber::from(1u8), Instant::now());
+
+        // Act
+        let range = acks.pop_range();
+        let empty = acks.pop_range();
+
+        //Assert
+        assert_eq!(range, Some(AcknowledgementRange::new(1u8.into(), 1u8.into())));
+        assert_eq!(empty, None);
+    }
+
+    #[test]
+    fn outgoing_acks_pop_range_one_range_start_end_different() {
+        // Arrange
+        let mut acks = OutgoingAcknowledgements::new();
+        acks.insert(DatagramSequenceNumber::from(1u8), Instant::now());
+        acks.insert(DatagramSequenceNumber::from(2u8), Instant::now());
+        acks.insert(DatagramSequenceNumber::from(3u8), Instant::now());
+
+        // Act
+        let range = acks.pop_range();
+        let empty = acks.pop_range();
+
+        //Assert
+        assert_eq!(range, Some(AcknowledgementRange::new(1u8.into(), 3u8.into())));
+        assert_eq!(empty, None);
+    }
+
+    #[test]
+    fn outgoing_acks_pop_range_multiple_ranges() {
+        // Arrange
+        let mut acks = OutgoingAcknowledgements::new();
+        acks.insert(DatagramSequenceNumber::from(1u8), Instant::now());
+
+        acks.insert(DatagramSequenceNumber::from(5u8), Instant::now());
+        acks.insert(DatagramSequenceNumber::from(6u8), Instant::now());
+        acks.insert(DatagramSequenceNumber::from(7u8), Instant::now());
+
+        acks.insert(DatagramSequenceNumber::from(10u8), Instant::now());
+        acks.insert(DatagramSequenceNumber::from(11u8), Instant::now());
+
+        acks.insert(DatagramSequenceNumber::from(20u8), Instant::now());
+
+        // Act
+        let range1 = acks.pop_range();
+        let range2 = acks.pop_range();
+        let range3 = acks.pop_range();
+        let range4 = acks.pop_range();
+        let empty = acks.pop_range();
+
+        //Assert
+        assert_eq!(range1, Some(AcknowledgementRange::new(1u8.into(), 1u8.into())));
+        assert_eq!(range2, Some(AcknowledgementRange::new(5u8.into(), 7u8.into())));
+        assert_eq!(range3, Some(AcknowledgementRange::new(10u8.into(), 11u8.into())));
+        assert_eq!(range4, Some(AcknowledgementRange::new(20u8.into(), 20u8.into())));
+        assert_eq!(empty, None);
+    }
+
+    #[test]
+    fn outgoing_acks_should_send_acks_initial_state() {
+        // Arrange
+        let acks = OutgoingAcknowledgements::new();
+
+        // Act/Assert
+        assert!(!acks.should_send_acks(Instant::now()));
+    }
+
+    #[test]
+    fn outgoing_acks_should_send_acks_one_number() {
+        // Arrange
+        let mut acks = OutgoingAcknowledgements::new();
+        let time = Instant::now();
+        acks.insert(DatagramSequenceNumber::from(1u8), time);
+
+        // Act/Assert
+        assert!(!acks.should_send_acks(time + (TIME_BEFORE_SENDING_ACKS - Duration::from_millis(1))));
+        assert!(!acks.should_send_acks(time + TIME_BEFORE_SENDING_ACKS));
+        assert!(acks.should_send_acks(time + (TIME_BEFORE_SENDING_ACKS + Duration::from_millis(1))));
+    }
+
+    #[test]
+    fn outgoing_acks_should_send_acks_multiple_numbers() {
+        // Arrange
+        let mut acks = OutgoingAcknowledgements::new();
+        let time = Instant::now();
+        acks.insert(DatagramSequenceNumber::from(1u8), time);
+        acks.insert(DatagramSequenceNumber::from(2u8), time + Duration::from_millis(100));
+        acks.insert(DatagramSequenceNumber::from(10u8), time + Duration::from_millis(200));
+        acks.insert(DatagramSequenceNumber::from(11u8), time + Duration::from_millis(300));
+
+        // Act/Assert
+        assert!(!acks.should_send_acks(time + (TIME_BEFORE_SENDING_ACKS - Duration::from_millis(1))));
+        assert!(!acks.should_send_acks(time + TIME_BEFORE_SENDING_ACKS));
+        assert!(acks.should_send_acks(time + (TIME_BEFORE_SENDING_ACKS + Duration::from_millis(1))));
+    }    
 }
